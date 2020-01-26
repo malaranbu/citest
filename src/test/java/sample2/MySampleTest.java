e0d53f654fa0f5270e4fa82d326190b74b7ee5df
malaranbu <malar.cse@gmail.com>
2020-01-26 17:44:16 +0000

    Project creation sample2

---
+++ src/test/java/sample2/MySampleTest.java
@@ -0,0 +1,13 @@
+package sample2;
+
+import org.junit.*;
+
+public class MySampleTest {
+	
+	@Test
+	public void MyTest1() {
+		System.out.println("Testing");
+		Assert.assertEquals("Test1", "Test1");
+	}
+
+}
